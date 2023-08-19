89e41dfd3099b67abe730abbb6001fdd22284cd8
Ayush Raj <ayushraj1268@gmail.com>
2023-08-19 18:54:19 +0530

    checking

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const database = require("./db/db.js");
 
 const app = express();
 app.use(bodyParser.json({ extended: true }));
-var corsOptions = {
-  origin: "http://localhost:3000",
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+// };
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header({ "Access-Control-Allow-Origin": "*" });
   next();
 });
-// app.use(cors());
-app.use("/", Routes);
+app.use(cors());
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -26,12 +26,15 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 database.connect(username, password);
+
 app.get("/", (req, res) => {
   return res.json({
     success: true,
     message: "Your server is up and running....",
   });
 });
+
+app.use("/", Routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
